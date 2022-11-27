79137dc2f27e4641fc457f3c115ab658dca48ea0
damatie <edafemaxwell@gmail.com>
2022-11-27 11:58:13 +0100

    fix: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueLight: "#1875F0",
+        blue: "#1860EC",
+        lightMustard: "#f4e064",
+        peach: "#ffa07a",
+        grayish: "#424242",
+        grayish2: "#4b4b4b",
+        grayish3: "#5a5a5a",
+        whiteGray:"#4b4b4b"
+      },
+      fontFamily: {
+        "Roboto": ["Roboto", "sans-serif"]
+      },
+    },
   },
-  plugins: [],
+ plugins: [require("daisyui")],
+  daisyui: { 
+    base: false,
+   
+  }
 }
