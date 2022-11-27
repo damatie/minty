87ca2b5af7424f06d0e271aff41af782de273499
damatie <edafemaxwell@gmail.com>
2022-11-27 14:54:42 +0100

    feat:create Side Nav

--- src/components/shared/SideNav.jsx
+++ src/components/shared/SideNav.jsx
@@ -1,5 +1,33 @@
+import { sideMenuLinks } from "../utils/Constants"
+import { Button } from "../Button"
 export const SideNav = () => {
+
+  console.log(sideMenuLinks)
   return (
-    <>Side Nav</>
+    <nav className=" flex flex-col pt-8">
+      <Button/>
+      <div>
+        <ul className=" space-y-2 text-sm mt-8">
+          {sideMenuLinks && sideMenuLinks.map((item, i) => (
+            <span className=" block w-full space-y-1 ">
+              <p className={`pl-8 text-[#647787] text-[11px] ${item.Parent===''? 'mt-3':'mt-5'}`}>{item.Parent}</p>
+              {item.links && item.links.map((obj, index) => (
+                <li key={index} className={` cursor-pointer text-[#647787] text-[11px] ${obj.isActive? "bg-[#E8F1FD] border-l-blueLight" :' hover:text-blueLight'} pl-[18px] py-2 border-l-4  `}>
+                <a href="/#" className="space-x-3">
+                  <span className=" float-left inline-block text-xl mt-[1px]">
+                    <img alt="icon" src={obj.icon}  className="  w-full object-contain"/>
+                  </span>
+                  <span className="  inline-block">{obj.name} </span>
+                </a>
+              </li>
+              ))}
+            </span>
+            ))
+          }
+        </ul>
+        
+      </div>
+
+    </nav>
   )
 }
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,8 +1,24 @@
+import { TopNav } from "../components/shared/TopNav"
+
 export const AppLayout = (props) => {
   return (
-    <div>
-      layout
-      {props.children}
+    <div className='
+      flex
+      flex-col
+      h-screen
+      w-screen
+      overflow-x-hidden
+      overflow-y-hidden'>
+      <div className=" w-full shadow">
+        <TopNav />
+      </div>
+      <div className=' flex flex-row bg-offBlue h-full w-full mt-1'>
+        <div className='w-[280px]  overflow-y-auto h-screen bg-slate-500 '>
+        </div>
+        <div className=" flex-1 h-full overflow-y-auto bg-gray-400  px-10 pt-10 pb-20 mb-20">
+          {props.children}
+        </div>
+      </div>
     </div>
   )
 }
