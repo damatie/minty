9c7d99a924de33370a20955bc7a328054f48005c
damatie <edafemaxwell@gmail.com>
2022-11-27 01:09:23 +0100

    feat: create dashboard screen

---
+++ src/screens/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { AppLayout } from "../layouts/AppLayout"
+
+export const DashboardScreen = () => {
+  return (
+    <AppLayout>
+      <main className=" bg-green-400">
+        screen
+      </main>
+    </AppLayout>
+  )
+}
