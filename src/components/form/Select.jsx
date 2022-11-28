f779d2cb28a959235a16947da2e4a6cc5bbb24e7
damatie <edafemaxwell@gmail.com>
2022-11-28 02:08:26 +0100

    fix:update select input component

---
+++ src/components/form/Select.jsx
@@ -0,0 +1,22 @@
+export const Select = ({
+  selectContainer,
+  onChange,
+  selectStyle,
+  list,
+  value,
+  label
+}) => {
+  return (
+    <>
+     <span className={`cursor-pointer   flex py-2 text-sm text-dark border-gray-400 ${selectContainer}`}>
+          <select onChange={onChange} className={`capitalize text-sm text-bluePrimary  outline-none border-0 ${selectStyle}`}>
+            <option value='' className=" ">{label}</option>
+              {list&&list.map((item, index) => (
+                <option value={item} key={index}>
+                  {item}
+                </option>
+      ))}
+          </select>
+      </span></>
+  )
+}
