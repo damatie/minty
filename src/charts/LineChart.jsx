65f45f9719174cf16e052dcbb997225cb4444083
damatie <edafemaxwell@gmail.com>
2022-11-28 02:54:36 +0100

    fix: chart

--- src/charts/LineChart.jsx
+++ src/charts/LineChart.jsx
@@ -12,27 +12,22 @@ export const data = [
     {
       name: "Jan 2019",
       "Product A": 3432,
-      "Procuct B": 2342
     },
     {
       name: "Feb 2019",
       "Product A": 2342,
-      "Procuct B": 7746
     },
     {
       name: "Mar 2019",
       "Product A": 4565,
-      "Procuct B": 2556
     },
     {
       name: "Apr 2019",
       "Product A": 6654,
-      "Procuct B": 4465
     },
     {
       name: "May 2019",
       "Product A": 8765,
-      "Procuct B": 5553
     }
 ];
 
