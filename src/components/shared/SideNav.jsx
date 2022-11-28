7e471eeb83f0aa945bed528bbe604bb0a195aee7
damatie <edafemaxwell@gmail.com>
2022-11-28 02:08:51 +0100

    fix:update side nav layout

--- src/components/shared/SideNav.jsx
+++ src/components/shared/SideNav.jsx
@@ -9,7 +9,7 @@ export const SideNav = () => {
       <div>
         <ul className=" space-y-2 text-sm mt-8">
           {sideMenuLinks && sideMenuLinks.map((item, i) => (
-            <span className=" block w-full space-y-1 ">
+            <span key={i} className=" block w-full space-y-1 ">
               <p className={`pl-8 text-[#647787] text-[11px] ${item.Parent===''? 'mt-3':'mt-5'}`}>{item.Parent}</p>
               {item.links && item.links.map((obj, index) => (
                 <li key={index} className={` cursor-pointer text-[#647787] text-[11px] ${obj.isActive? "bg-[#E8F1FD] border-l-blueLight" :' hover:text-blueLight'} pl-[18px] py-2 border-l-4  `}>
