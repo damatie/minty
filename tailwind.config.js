e67779f288c41910623c5ae5f013c278f2a614f9
damatie <edafemaxwell@gmail.com>
2022-11-28 02:06:08 +0100

    chore:m install chart

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,20 @@ module.exports = {
     extend: {
       colors: {
         blueLight: "#1875F0",
-        blue: "#1860EC",
-        lightMustard: "#f4e064",
-        peach: "#ffa07a",
-        grayish: "#424242",
-        grayish2: "#4b4b4b",
-        grayish3: "#5a5a5a",
-        whiteGray:"#4b4b4b"
+        bluePrimary: "#1860EC",
+        action: "#27AE60",
+        bgMain: "#E5E5E5",
+        dark: "#262626",
+        darker: "#4F4F4F", 
+        primaryGreen:'#27AE60',
+        grayPrimary: "#787C90",
+        yelloPrimary:'#FDC203',
+        whiteGray: "#7F8FA4",
+        tableHead: "#EAEEF0"
       },
       fontFamily: {
-        "Roboto": ["Roboto", "sans-serif"]
+        "Roboto": ["Roboto", "sans-serif"],
+        "Segoe-UI-Bold": ["Segoe-UI-Bold", "sans-serif"],
       },
     },
   },
